1a0150d7b1715a753f29723816fb3b02509af48e
Ramesh Borukati <96637945+rameshbigdata@users.noreply.github.com>
2021-12-31 07:00:49 +0530

    Add files via upload

---
+++ KPI9/DeptAvgSatisWorkhrLeftEmpnotprmMapper_KPI9.java
@@ -0,0 +1,27 @@
+package com.ramesh.hranalysis; 
+
+	import java.io.IOException;
+
+	import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+	public class DeptAvgSatisWorkhrLeftEmpnotprmMapper_KPI9 extends Mapper<LongWritable, Text, Text, Text>
+	{
+		public final void map(final LongWritable key, final Text value, final Context context) throws IOException, InterruptedException
+		{
+			final String line = value.toString(); 
+//		satisfaction_level,last_evaluation,number_project,average_montly_hours,time_spend_company,Work_accident,left,promotion_last_5years,Department,salary
+//		0.38,0.53,2,157,3,0,1,0,sales,low
+
+		final String[] data = line.trim().split(",");
+			
+			if ((data.length == 10) && (!data[0].contains("satisfaction_level"))  && (data[9].contains("low")) && (Integer.parseInt(data[7])==0))				// check length of the data
+			{					
+				final String dept =data[8];
+				context.write(new Text (dept), new Text(data[0]+','+data[3]+','+data[6]));
+//				(ConstantKey,214.05) output to reducer
+			}
+		}
+	}
---
+++ KPI9/DeptAvgSatisWorkhrLeftEmpnotprmReducer_KPI9.java
@@ -0,0 +1,38 @@
+package com.ramesh.hranalysis; 
+
+	import java.io.IOException;
+import java.text.DecimalFormat;
+
+	import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+	public class DeptAvgSatisWorkhrLeftEmpnotprmReducer_KPI9 extends Reducer<Text, Text, Text, Text>
+	{
+		public final void reduce(final Text key, final Iterable<Text> values, final Context context) throws IOException, InterruptedException
+		{
+//Department wise average satisfaction level, average working hours and no of employee who left company.
+			 
+			int count = 0;
+			double avgSatis=0.0;
+			int avgWrk=0;
+			int left=0;
+//			Key => department  Value => 214.05,100.00,200.00,300.00.....
+			for (final Text val : values)
+			{
+				String[] value = val.toString().split(",");			
+				count++;
+				avgSatis=avgSatis+Double.parseDouble(value[0]);
+				avgWrk=avgWrk+Integer.parseInt(value[1]);
+				if (Integer.parseInt(value[2])==1)
+				{
+					left++;
+				}
+			} 
+			String avgSatisfaction=String.valueOf(avgSatis/count);
+			String avgWrking=String.valueOf(avgWrk/count);
+			String lef=String.valueOf(left);
+			context.write(key, new Text(avgSatisfaction+'\t'+avgWrking+'\t'+lef)); 
+		}
+	}
